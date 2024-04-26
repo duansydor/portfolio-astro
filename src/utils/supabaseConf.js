6e04e8a3505eea20dbc46eb28f48bf5fd3976041
Duan <josiasduan@gmail.com>
2024-04-25 21:17:39 -0300

    Styles fixed, projects page and database setup

--- src/utils/supabaseConf.js
+++ src/utils/supabaseConf.js
@@ -3,3 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(import.meta.env.SUPABASE_API, import.meta.env.SUPABASE_ANON_KEY)
+
+export const getFile = (fileName) => {
+    const { data } = supabase.storage.from('portfolioblog').getPublicUrl(fileName)
+    return data.publicUrl
+}
